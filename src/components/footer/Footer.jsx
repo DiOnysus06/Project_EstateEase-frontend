d7d0c6d84751a8ed37e597c5aa9c18e3f55a71bd
DIVYANSHU <135935472+DiOnysus06@users.noreply.github.com>
2024-06-09 10:33:33 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="info">
+        <div className="terms">
+            Terms and Conditions
+        </div>
+        <div className="contacts">
+            contact us on +1 561-555-7689
+        </div>
+        <div className="privacy">
+            Privacy and policy
+        </div>
+        <div className="copy">
+            copyright  Ⓒ 2023 EstateEase
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
